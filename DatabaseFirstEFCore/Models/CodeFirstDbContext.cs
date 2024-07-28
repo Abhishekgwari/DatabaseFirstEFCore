d88ab2769d86a7fb7f2861d70ec49adb1eccc140
Abhishek <Abhishekgwari14sep@gmail.com>
2024-07-28 15:46:52 +0530

    Add project files.

---
+++ DatabaseFirstEFCore/Models/CodeFirstDbContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseFirstEFCore.Models;
+
+public partial class CodeFirstDbContext : DbContext
+{
+    public CodeFirstDbContext()
+    {
+    }
+
+    public CodeFirstDbContext(DbContextOptions<CodeFirstDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Designation)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nmae)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("Student Gender");
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("Student Name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
